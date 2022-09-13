3da5b0b93ad33e0484e364db9e637e6d04c75a38
Masha Kuznetsova <kmp2535@yandex.ru>
2022-09-13 22:34:08 +0700

    Project 15

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,27 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit'); 
+const helmet = require('helmet'); 
 
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const authRouter = require('./routes/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger'); 
 
 const NotFoundError = require('./errors/not-found-err');
 const { auth } = require('./middlewares/auth');
+
+const corsOptions = { 
+  origin: ['https://marysmith.nomoredomains.sbs', 'http://marysmith.nomoredomains.sbs'], 
+  credentials: true, 
+};
+ 
+const limiter = rateLimit({ 
+  windowMs: 60 * 1000, 
+  max: 30,
+}); 
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -22,8 +36,18 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
+app.use(cors(corsOptions)); 
+app.use(limiter); 
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(requestLogger); 
+
+app.get('/crash-test', () => { 
+  setTimeout(() => { 
+    throw new Error('Сервер сейчас упадёт'); 
+  }, 0); 
+}); 
 
 app.use(authRouter);
 
@@ -36,6 +60,7 @@ app.use((req, res, next) => {
   next(new NotFoundError('По данному запросу api не найден'));
 });
 
+app.use(errorLogger); 
 app.use(errors());
 
 app.use((err, req, res, next) => {
