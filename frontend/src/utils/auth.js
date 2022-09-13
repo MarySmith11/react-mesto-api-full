96aa56965c8cc400830ab8ae7833515cce08ecc8
Masha Kuznetsova <kmp2535@yandex.ru>
2022-09-13 23:13:43 +0700

    Project 15 domain

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'https://backend.marysmith.nomoredomains.sbs';
 
 const getResponse = (response) => {
   if (response.ok && response.status !== 400 && response.status !== 401) {
