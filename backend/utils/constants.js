a7073dc13ead160b4045f19f4ea0f7f5f1fca94a
John Doe <johndoe@example.com>
2022-09-18 20:42:16 +0300

    project 15 fix

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,1 +1,1 @@
-module.exports.urlRegexp = new RegExp('ˆ(?:http(s)?:\\/\\/)?[\\w.-]+(?:\\.[\\w.-]+)+[\\w\\-._:/?#[\\]@!$&\'()*+,;=.]+$');
+module.exports.urlRegexp = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/);
